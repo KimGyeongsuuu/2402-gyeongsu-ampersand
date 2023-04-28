866106811ffb40838c8aa02c6e451b7a6e1e85e3
KimGyeongsuuu <sk62628504@gmail.com>
2023-04-28 23:22:05 +0900

    project start

---
+++ src/test/java/com/example/ampersand/AmpersandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ampersand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpersandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
